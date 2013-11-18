985903c3710ad98025599ae33154a6d52cbb6ae8
Srinidhi J <srinidhij.21@gmail.com>
2013-11-18 10:09:29 +0530

    Added poll based monitor

--- monitor/monitor.py
+++ monitor/monitor.py
@@ -1,9 +1,21 @@
+import sys
 import select
 import time
 import socket
 from platform import platform
 
+class EPollNotImplemented(Exception):
+    ''' Exception class for epoll not being supported 
+    for the platform'''
+    pass
+
+class KQueueNotImplemented(Exception):
+    ''' Exception class for kqueue not being supported 
+    for the platform'''
+    pass
+
 BUF_SIZE = 1024*1024
+
 class epolludprecieve:
 
     def __init__(self, addr):
@@ -16,8 +28,12 @@ class epolludprecieve:
             self.epoll = select.epoll()
             self.epoll.register(self.fileno, select.EPOLLIN)
 
-        except :
-            raise
+        except AttributeError:
+            raise EPollNotImplemented()
+
+        except Exception, e:
+            sys.stderr.write(str(e))
+
 
     def recieve(self):
         data = None
@@ -43,28 +59,56 @@ class epolludprecieve:
 
 class polludprecieve:
     
-    def __init__(self):
-        poll = select.poll()
-        #poll.register()
+    def __init__(self, addr):
+        try:
+            self.commsock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            self.commsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            self.commsock.setblocking(0)
+            self.commsock.bind(addr)
+            self.fileno = self.commsock.fileno()
+            self.poll = select.poll()
+            self.poll.register(self.fileno, select.POLLIN)
 
-    def recieve(self, data):
-        pass
+        except Exception, e:
+            sys.stderr.write(str(e))
+
+    def recieve(self):
+        data = None
+        try:
+            events = self.poll.poll(0)
+            for fileno, event in events:
+                if event & select.POLLIN:
+                    data, addr = self.commsock.recvfrom(BUF_SIZE)
+        except ValueError:
+            pass
+        except KeyboardInterrupt:
+            return data
+        finally:
+            pass
+        return data 
 
 class kqueueudprecieve:
     
     def __init__(self, addr):
-        self.kqueue = select.kqueue
-        self.kevent = select.kevent
-        self.commsock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.commsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.commsock.setblocking(0)
-        self.commsock.bind(addr)
-        self.fileno = self.commsock.fileno()
-        self.kq = self.kqueue()
-        self.event = [self.kevent(self.fileno, filter=select.KQ_FILTER_READ, flags=select.KQ_EV_ADD)]
+        try:
+            self.kqueue = select.kqueue
+            self.kevent = select.kevent
+            self.commsock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            self.commsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            self.commsock.setblocking(0)
+            self.commsock.bind(addr)
+            self.fileno = self.commsock.fileno()
+            self.kq = self.kqueue()
+            self.event = [self.kevent(self.fileno, filter=select.KQ_FILTER_READ, flags=select.KQ_EV_ADD)]
 
-        self.events = self.kq.control(self.event, 0, 0)
-        #poll.register()
+            self.events = self.kq.control(self.event, 0, 0)
+
+        except AttributeError:
+            raise KQueueNotImplemented()
+
+        except Exception, e:
+            sys.stderr.write(str(e))
+            #poll.register()
 
     def recieve(self):
         data = None
@@ -93,7 +137,6 @@ def getreciever(addr):
         try:
             return kqueueudprecieve(addr)
         except:
-            raise
             return polludprecieve(addr)
     else:
         return polludprecieve(addr)
@@ -103,7 +146,7 @@ addr = ('localhost', 8888)
 reciever = getreciever(addr)
 
 def recieve():
-        return reciever.recieve()
+    return reciever.recieve()
 
 def tests():
     i = 0
