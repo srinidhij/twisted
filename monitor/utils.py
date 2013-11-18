3068f0d4f9cbcf3780678b7cf43d6d8e5ad16d24
Srinidhi J <srinidhij.21@gmail.com>
2013-11-18 10:10:47 +0530

    Added utils for random data gen

---
+++ monitor/utils.py
@@ -0,0 +1,15 @@
+from random import random, choice, randint
+import string
+
+events = ['listenTCP', 'connectTCP', 'listenSSL', 'connectionLost', 'spawnProcess', 'listenUDP']
+levels = ['info', 'critical', 'warning']
+
+def genrandstr():
+    l = randint(5,20)
+    return { 'event': choice(events),
+    'info' : ''.join([choice(string.ascii_lowercase) for i in xrange(l)]), 
+    'level' : choice(levels)
+    }
+
+def sleeptime(scale=3):
+    return scale*random()
