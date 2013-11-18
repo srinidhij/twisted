e2cc79145ef56cd296d713efb77f03827bd63a2b
Srinidhi J <srinidhij.21@gmail.com>
2013-11-18 10:08:41 +0530

    Added poll based sender

--- monitor/nonblockingudpsender.py
+++ monitor/nonblockingudpsender.py
@@ -1,13 +1,19 @@
+import sys
 import select
 import socket
 from platform import platform
 from time import sleep
-from random import random, choice, randint
-import string
 
-def genrandstr():
-    l = randint(5,100)
-    return ''.join([choice(string.ascii_lowercase) for i in xrange(l)])
+class EPollNotImplemented(Exception):
+    ''' Exception class for epoll not being supported 
+    for the platform'''
+    pass
+
+class KQueueNotImplemented(Exception):
+    ''' Exception class for kqueue not being supported 
+    for the platform'''
+    pass
+
 
 class epolludpsend:
 
@@ -20,8 +26,12 @@ class epolludpsend:
             self.epoll = select.epoll()
             self.epoll.register(self.fileno, select.EPOLLOUT)
 
-        except :
-            raise
+        except AttributeError:
+            raise EPollNotImplemented()
+
+        except Exception, e:
+            sys.stderr.write(str(e))
+
     def send(self, addr, data):
         bwriten = 0
         try :
@@ -40,26 +50,54 @@ class epolludpsend:
 class polludpsend:
     
     def __init__(self):
-        poll = select.poll()
-        #poll.register()
+        try:
+            self.commsock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            self.commsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            self.commsock.setblocking(0)
+            self.commsock.bind(addr)
+            self.fileno = self.commsock.fileno()
+            self.poll = select.poll()
+            self.poll.register(self.fileno, select.POLLOUT)
 
-    def send(self, data):
-        pass
+        except Exception, e:
+            sys.stderr.write(str(e))
+
+    def recieve(self, addr, data):
+        bwriten = 0
+        try:
+            events = self.poll.poll(0)
+            for fileno, event in events:
+                if event & select.POLLOUT:
+                    bwriten = self.commsock.sendto(data, addr)
+        except ValueError:
+            pass
+        except KeyboardInterrupt:
+            return bwriten
+        finally:
+            pass
+        return bwriten
 
 class kqueueudpsend:
     
     def __init__(self):
-        self.kqueue = select.kqueue
-        self.kevent = select.kevent
-        self.commsock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.commsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.commsock.setblocking(0)
-        self.fileno = self.commsock.fileno()
-        self.kq = self.kqueue()
-        self.event = [self.kevent(self.fileno, filter=select.KQ_FILTER_WRITE, flags=select.KQ_EV_ADD)]
+        try:
+            self.kqueue = select.kqueue
+            self.kevent = select.kevent
+            self.commsock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            self.commsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            self.commsock.setblocking(0)
+            self.fileno = self.commsock.fileno()
+            self.kq = self.kqueue()
+            self.event = [self.kevent(self.fileno, filter=select.KQ_FILTER_WRITE, flags=select.KQ_EV_ADD)]
 
-        self.events = self.kq.control(self.event, 0, 0)
+            self.events = self.kq.control(self.event, 0, 0)
         #poll.register()
+        except AttributeError:
+            raise KQueueNotImplemented()
+
+        except Exception, e:
+            sys.stderr.write(str(e))
+
 
     def send(self, addr, data):
         bwriten = 0
@@ -68,6 +106,9 @@ class kqueueudpsend:
             for event in w_events:
                 if event.filter == select.KQ_FILTER_WRITE:
                     bwriten = self.commsock.sendto(data, addr)
+
+        except Exception, e:
+            sys.stderr.write(str(e))
         finally:
             #self.commsock.shutdown(SHUT_WR)
             #self.commsock.close()
@@ -88,7 +129,6 @@ def getsender():
         try:
             return kqueueudpsend()
         except:
-            raise
             return polludpsend()
     else:
         return polludpsend()
@@ -100,10 +140,11 @@ def send(addr, data):
     sender.send(addr, data)
 
 def tests():
+    from utils import sleeptime, genrandstr
     addr = ('localhost', 8888)
     while  True:
-        sleep(random())
-        send(addr , genrandstr())
+        sleep(sleeptime())
+        send(addr , str(genrandstr()))
 
 if __name__ == '__main__':
     tests()
