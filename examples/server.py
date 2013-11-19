41ce4857661a50b56c5e7da176eedc5765ccb8eb
Srinidhi J <srinidhij.21@gmail.com>
2013-11-19 14:54:27 +0530

    Fixed a small bug

--- examples/server.py
+++ examples/server.py
@@ -1,10 +1,9 @@
+import sys
 from twisted.web.server import Site
 from twisted.web.static import File
 from twisted.internet import reactor
-import sys
 
-
-resource = sys.argv[1] if len(sys.argv) > 1 else File('/Users/srinidhi')
+resource = File(sys.argv[1]) if len(sys.argv) > 1 else File('/Users/srinidhi')
 factory = Site(resource)
 reactor.listenTCP(8888, factory)
 reactor.run()
